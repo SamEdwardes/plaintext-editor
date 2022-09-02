df2ca44a5315e834654f56e0638d3732fdcff5b4
SamEdwardes <edwardes.s@gmail.com>
2022-09-02 10:41:26 -0700

    Looking good

---
+++ components/TheTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 mt-4">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+        <div>
+          <span class="inline">Plain Text </span>
+          <span class="inline text-indigo-600">Editor</span>
+        </div>
+      </h1>
+      <p class="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
+        A simple online no frills plain text editor.
+      </p>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <TextArea class="relative"></TextArea>
+  <div class="flex justify-center">
+    <div class="w-[800px]">
+      <ButtonBase @click="marginOnRight = !marginOnRight" class="basis-1/6 text-right">Space on Right</ButtonBase>
+      <div class="mt-1" :class="marginOnRight ? 'mr-96' : ''">
+        <textarea 
+          rows="30" 
+          name="comment" 
+          id="comment" 
+          :class="[
+            'block w-full rounded-md border-gray-300 shadow-lg',
+            'focus:border-indigo-500 focus:ring-indigo-500',
+            'sm:text-sm font-mono'
+          ]" 
+        />
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+  const marginOnRight = ref(false)
+</script>
