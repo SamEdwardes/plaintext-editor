b3f94ae36383a4e63ad57093b2b8b8448c2cc499
SamEdwardes <edwardes.s@gmail.com>
2022-09-02 11:54:29 -0700

    Ready to deploy!

--- pages/index.vue
+++ pages/index.vue
@@ -2,46 +2,47 @@
   <div>
     <div class="flex justify-center mt-8">
       <div class="w-[800px]">
-        <!-- <ButtonBase @click="marginOnRight = !marginOnRight" class="basis-1/6 text-right">Space on Right</ButtonBase> -->
-        <div class="
-          flex flex-row 
-          shadow-lg
-          border-gray-300
-        ">
-          <div class="
-          bg-gray-300 
-          rounded-l-lg w-8
-          flex flex-col text-center divide-y-2 divide-gray-400/25
-          ">
+        <div class="flex flex-row border-gray-300">
+          <!-- Sidebar -->
+          <div class="w-8 flex flex-col bg-gray-300 rounded-l-lg">
             <button @click="marginOnRight = !marginOnRight" class="m-1">
-              <ChevronDoubleLeft v-if="!marginOnRight"/>
-              <ChevronDoubleRight v-else />
+              <IconChevronDoubleLeft v-if="!marginOnRight"/>
+              <IconChevronDoubleRight v-else />
+            </button>
+            <button @click="copyToClipBoard" class="m-1 pb-96">
+              <IconClipboard />
             </button>
           </div>
+          <!-- Text editor -->
           <div class="w-full" :class="marginOnRight ? 'mr-96' : ''">
             <textarea
               rows="30"
-              name="comment"
-              id="comment"
+              name="plainText"
+              id="plain-text"
+              v-model="plainText"
               class="
                 block w-full rounded-r-lg sm:text-sm font-mono
-                focus:border-0 focus:ring-0
+                 focus:border-gray-300
+                focus:ring-0
                 border-gray-300
                 border-l-0
               "
-            />
+            >
+              {{ plainTextCookie }}
+            </textarea>
           </div>
         </div>
       </div>
     </div>
-    <div>xxx</div>
-    <ChevronDoubleRight />
   </div>
 </template>
 
 <script setup>
-import ArrowLeft from "../components/Icon/ArrowLeft.vue";
-import ChevronDoubleLeft from "../components/Icon/ChevronDoubleLeft.vue";
-import ChevronDoubleRight from "../components/Icon/ChevronDoubleRight.vue";
   const marginOnRight = ref(false)
+  const plainText = ref('')
+
+  function copyToClipBoard() {
+    console.log(plainText.value)
+    navigator.clipboard.writeText(plainText.value)
+  }
 </script>
