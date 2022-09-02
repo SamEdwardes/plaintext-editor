31987be619ca3877b337748d50e3659fa67455cc
SamEdwardes <edwardes.s@gmail.com>
2022-09-02 11:17:37 -0700

    Good but big border

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,47 @@
 <template>
-  <div class="flex justify-center">
-    <div class="w-[800px]">
-      <ButtonBase @click="marginOnRight = !marginOnRight" class="basis-1/6 text-right">Space on Right</ButtonBase>
-      <div class="mt-1" :class="marginOnRight ? 'mr-96' : ''">
-        <textarea 
-          rows="30" 
-          name="comment" 
-          id="comment" 
-          :class="[
-            'block w-full rounded-md border-gray-300 shadow-lg',
-            'focus:border-indigo-500 focus:ring-indigo-500',
-            'sm:text-sm font-mono'
-          ]" 
-        />
+  <div>
+    <div class="flex justify-center mt-8">
+      <div class="w-[800px]">
+        <!-- <ButtonBase @click="marginOnRight = !marginOnRight" class="basis-1/6 text-right">Space on Right</ButtonBase> -->
+        <div class="
+          flex flex-row 
+          shadow-lg
+          border-gray-300
+        ">
+          <div class="
+          bg-gray-300 
+          rounded-l-lg w-8
+          flex flex-col text-center divide-y-2 divide-gray-400/25
+          ">
+            <button @click="marginOnRight = !marginOnRight" class="m-1">
+              <ChevronDoubleLeft v-if="!marginOnRight"/>
+              <ChevronDoubleRight v-else />
+            </button>
+          </div>
+          <div class="w-full" :class="marginOnRight ? 'mr-96' : ''">
+            <textarea
+              rows="30"
+              name="comment"
+              id="comment"
+              class="
+                block w-full rounded-r-lg sm:text-sm font-mono
+                focus:border-0 focus:ring-0
+                border-gray-300
+                border-l-0
+              "
+            />
+          </div>
+        </div>
       </div>
     </div>
+    <div>xxx</div>
+    <ChevronDoubleRight />
   </div>
 </template>
 
 <script setup>
+import ArrowLeft from "../components/Icon/ArrowLeft.vue";
+import ChevronDoubleLeft from "../components/Icon/ChevronDoubleLeft.vue";
+import ChevronDoubleRight from "../components/Icon/ChevronDoubleRight.vue";
   const marginOnRight = ref(false)
 </script>
